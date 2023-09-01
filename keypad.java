f4aa88009babd637bdf2d84a958b828187beacb3
Mina-ah <minaparveenn@gmail.com>
2023-09-01 21:21:01 +0530

    code for a phones keyboard

--- keypad.java
+++ keypad.java
@@ -11,6 +11,7 @@ public class keypad {
 key(str, idx+1, combination+mapping.charAt(i));
         }    
         }
+        //time complexity is 4^n because for each character its going to be 2 choices here 2 and 3 are two different characters so the time complexity will be 4 to the power n
     public static void main(String[] args){
         String str="23";
         key(str, 0, "");
